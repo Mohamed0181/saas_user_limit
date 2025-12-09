5ed3700324e518edc628fe9ff76037954d156ea2
Mohamed Riyad <mohamedreyad0181@gmail.com>
2025-12-09 11:09:14 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'saas base 1',
+    'name': 'saas base 2',
     'category': 'Odoo Management',
     'summary': 'Control odoo',
     'author': 'Optimum Smart Solutions',
